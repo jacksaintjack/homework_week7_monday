44adc582d30ac88eab00696c0c89a54d32464c8f
Nicholas Glover <jacksaintjack@gmail.com>
2015-11-18 15:30:12 -0600

    Added token.js and fixed an error with <li>

--- src/js/repositoryinfo.js
+++ src/js/repositoryinfo.js
@@ -4,16 +4,20 @@ class RepoInfo extends React.Component {
   render () {
     return(
       <div id="projectBox">
-        <li>
-          <a href ="#"> {this.props.repo.name}  </a>
-          <li id="stats"> {this.props.repo.language}
-            <span className="octicon octicon-star starIcon"></span>
-            <span> {this.props.repo.stargazers}</span>
-            <span className="octicon octicon-git-branch branchIcon"></span>
-            <span> {this.props.repo.forks}</span>
+        <ul>
+          <li>
+            <a href ="#"> {this.props.repo.name}  </a>
+            <ul>
+              <li id="stats"> {this.props.repo.language}
+                <span className="octicon octicon-star starIcon"></span>
+                <span> {this.props.repo.stargazers}</span>
+                <span className="octicon octicon-git-branch branchIcon"></span>
+                <span> {this.props.repo.forks}</span>
+              </li>
+            </ul>
+            <span className="updatedTime"> Updated {moment(" " +this.props.repo.updated_at).fromNow()}</span>
           </li>
-          <span className="updatedTime"> Updated {moment(" " +this.props.repo.updated_at).fromNow()}</span>
-        </li>
+        </ul>
     </div>
   );
 }
